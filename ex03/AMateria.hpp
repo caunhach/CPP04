997a196552cffdad12eefa33f2799eb16ffe2ba3
Chayapon Aunhachoke <caunhach@z1t1c2.42bangkok.com>
2023-12-04 23:07:16 +0700

    last updated 04/12/2023

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
 
+#include <iostream>
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+	protected:
+	public:
+		AMateria(std::string const & type);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
--- ex03/IMateriaSource.hpp
+++ ex03/IMateriaSource.hpp
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef IMATERIAL_HPP
+# define IMATERIAL_HPP
 
+#include "ICharacter.hpp"
+
+class IMateriaSource
+{
+	public:
+		virtual ~IMateriaSource() {}
+		virtual void learnMateria(AMateria*) = 0;
+		virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
